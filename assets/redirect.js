6126e36a9739bcc9f2a6ab0c149975dcdffd9876
Jan Krüger <jan@krueger-jan.de>
2019-11-01 12:21:59 +0100

    updating moving notice & redirection

---
+++ assets/redirect.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    if(window.location.hostname === 'starcitizen.guide')
+    {
+        window.location.replace('https://starfarer.space/' + window.location.search);
+    }
+});

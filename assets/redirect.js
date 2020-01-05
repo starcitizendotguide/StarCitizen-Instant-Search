0e7c904f0b26da5c05688b2d03eb78e542806253
Jan Krüger <jan@krueger-jan.de>
2020-01-05 15:36:06 +0100

    remove dependencies

--- assets/redirect.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    if(window.location.hostname === 'starcitizen.guide')
-    {
-        window.location.replace('https://starfarer.space/' + window.location.search);
-    }
-});
